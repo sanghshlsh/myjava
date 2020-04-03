7b980c015da019acb9533197d42cfeb8cd6654ef
sanghshlsh <sanghshlsh@gmail.com>
2020-04-03 17:48:32 +0900

    04-03

--- src/codingbat/String/String1.java
+++ src/codingbat/String/String1.java
@@ -115,4 +115,139 @@ public class String1 {
 		return false;
 	}
 
+	// atFirst
+	public String atFirst(String str) {
+		boolean run = true;
+		while (run) {
+			if (str.length() > 2)
+				run = false;
+			str += "@";
+		}
+		return str.substring(0, 2);
+	}
+
+	// lastChars
+	public String lastChars(String a, String b) {
+		String s = "";
+		if (a.length() == 0)
+			s += "@";
+		else
+			s += a.substring(0, 1);
+		if (b.length() == 0)
+			s += "@";
+		else
+			s += b.substring(b.length() - 1);
+		return s;
+	}
+
+	// conCat
+	public String conCat(String a, String b) {
+		if (a.length() > 0 && b.length() > 0) {
+			if (a.substring(a.length() - 1).equals(b.substring(0, 1)))
+				return a.substring(0, a.length() - 1) + b;
+			return a + b;
+		} else
+			return a + b;
+	}
+
+	// lastTwo
+	public String lastTwo(String str) {
+		int a = str.length();
+		if (a > 1)
+			return str.substring(0, a - 2) + str.substring(a - 1) + str.substring(a - 2, a - 1);
+		return str;
+	}
+
+	// seeColor
+	public String seeColor(String str) {
+		int a = str.length();
+		if (a > 2 && str.substring(0, 1).equals("r") && str.substring(1, 2).equals("e")
+				&& str.substring(2, 3).equals("d"))
+			return "red";
+		else if (a > 3 && str.substring(0, 1).equals("b") && str.substring(1, 2).equals("l")
+				&& str.substring(2, 3).equals("u") && str.substring(3, 4).equals("e"))
+			return "blue";
+		return "";
+	}
+
+	// frontAgain
+	public boolean frontAgain(String str) {
+		int i = str.length();
+		int j = i / 2;
+		if (j < 1)
+			return false;
+		return str.substring(0, 2).equals(str.substring(i - 2));
+	}
+
+	// minCat
+	public String minCat(String a, String b) {
+		int al = a.length();
+		int bl = b.length();
+		if (al > bl)
+			return a.substring(al - bl) + b;
+		return a + b.substring(bl - al);
+	}
+
+	// extraFront
+	public String extraFront(String str) {
+		if (str.length() > 2)
+			str = str.substring(0, 2);
+		return str + str + str;
+	}
+
+	// without2
+	public String without2(String str) {
+		int i = str.length();
+		if (i < 2)
+			return str;
+		if (str.substring(0, 2).equals(str.substring(i - 2)))
+			return str.substring(2);
+		return str;
+	}
+
+	// deFront
+	public String deFront(String str) {
+		int i = str.length();
+		String result = "";
+		switch (i) {
+		case 0:
+			return result;
+		case 1:
+			return str.equals("a") ? str : result;
+		default:
+			if (str.charAt(1) == 'b')
+				result = "b" + result;
+			if (str.charAt(0) == 'a')
+				result = "a" + result;
+			result += str.substring(2);
+			return result;
+		}
+	}
+
+	// startWord
+	public String startWord(String str, String word) {
+		int sl = str.length();
+		int wl = word.length();
+		if (sl < wl)
+			return "";
+		if (str.substring(1, wl).equals(word.substring(1, wl)))
+			return str.substring(0, wl);
+		return "";
+	}
+
+	// withoutX
+	public String withoutX(String str) {
+		int l = str.length();
+		int start = 0;
+		if (l < 2)
+			return str.equals("x") ? "" : str;
+		if (str.charAt(0) == 'x')
+			start++;
+		if (str.charAt(l - 1) == 'x')
+			l--;
+		return str.substring(start, l);
+	}
+
+	
+	
 }
